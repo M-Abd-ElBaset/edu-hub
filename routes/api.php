23a513b0a01f17a9359f765dce94bee0b24096b1
Mohamed Elgebaly <36784871+M-Abd-ElBaset@users.noreply.github.com>
2025-10-31 10:05:49 +0200

    added courses api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::middleware(['auth.jwt', 'throttle:100,1'])->group(function () {
+    Route::post('/courses/{id}/enroll', [CourseController::class, 'enroll']);
+    Route::get('/courses', [CourseController::class, 'index']);
 });
